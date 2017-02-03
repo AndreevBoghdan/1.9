b09a394c2e65cf6d4518de0783abf5567904059c
Andreev Boghdan <boghdanandreev@gmail.com>
2017-02-03 16:35:40 +0200

    wsgi

--- man/wsgi.py
+++ man/wsgi.py
@@ -3,7 +3,6 @@ import sys
 from os import environ
 from django.core.handlers.wsgi import WSGIHandler
 
-sys.path = ['var/www/django-man-project'] + sys.path
 
 environ['DJANGO_SETTINGS_MODULE'] = environ.get(
     'DJANGO_SETTINGS_MODULE', 'settings')
